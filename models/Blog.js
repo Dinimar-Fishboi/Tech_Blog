4cd682868ba62c055122bc4689cbc9252514f758
Dinimar Fish <tayla@tallisman.com.au>
2021-09-09 21:11:58 +1000

    save for new Models

--- models/Blog.js
+++ models/Blog.js
@@ -1,12 +1,12 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-class Blog extends Model {}
+// class Blog extends Model {}
 
-Blog.init(
-    {
+// Blog.init(
+//     {
         
-    }
-)
+//     }
+// )
 
-module.exports = Blog;
+// module.exports = Blog;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
-const Comment = require('./Comment');
-const Blog = require('./Blog');
+// const Comment = require('./Comment');
+// const Blog = require('./Blog');
 
 
-module.exports = { User, Comment, Blog};
+module.exports = { User};
