e8256a7043c113229fc0ac62155f59b49089db9e
Dinimar Fish <tayla@tallisman.com.au>
2021-09-11 21:47:27 +1000

    updated Index to reflect model relationships

--- models/index.js
+++ models/index.js
@@ -1,6 +1,20 @@
 const User = require('./User');
-// const Comment = require('./Comment');
-// const Blog = require('./Blog');
+const Comment = require('./Comment');
+const Blog = require('./Blog');
 
+//User can have many Blog (posts)
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+})
 
-module.exports = { User};
+//A Blog (post) will only belong to one User
+Blog.hasOne(User, {
+    foreignKey: "user_id",
+})
+
+//A Blog may have many Comments
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+})
+
+module.exports = { User, Comment, Blog};
