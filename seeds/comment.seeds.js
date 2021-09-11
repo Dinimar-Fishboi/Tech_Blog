4643c03a366d5e5ba8cd7069d11cc8272ce6f1c8
Dinimar Fish <tayla@tallisman.com.au>
2021-09-11 22:31:10 +1000

    updated Seed file to run backend testing:

---
+++ seeds/comment.seeds.js
@@ -0,0 +1,23 @@
+const { Comment } = require('../models');
+
+const commentData = [
+    {
+      name: "Sal",
+      email: "sal@hotmail.com",
+      password: "password12345"
+    },
+    {
+      name: "Lernantino",
+      email: "lernantino@gmail.com",
+      password: "password12345"
+    },
+    {
+      name: "Amiko",
+      email: "amiko2k20@aol.com",
+      password: "password12345"
+    }
+  ];
+
+  const seedUser = () => User.bulkCreate(userData);
+
+  module.exports = seedUser;
