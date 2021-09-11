59f903971bbb4bfd410a2421625b14d3499ce580
Dinimar Fish <tayla@tallisman.com.au>
2021-09-11 21:35:55 +1000

    added content to Comment Model

--- models/Comment.js
+++ models/Comment.js
@@ -1,12 +1,32 @@
-// const { Model, DataTypes } = require('sequelize');
-// const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-// class Comment extends Model {}
+class Comment extends Model {}
 
-// Comment.init(
-//     {
-        
-//     }
-// )
+Comment.init(
+    {
+    comment_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    
+        }, 
+        blog_id: {
+            type: DataTypes.INTEGER,      
+            references: {
+              model: "blog",
+              key: 'id',
+            }
+    },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+      }
+);
 
-// module.exports = Comment;
+module.exports = Comment;
