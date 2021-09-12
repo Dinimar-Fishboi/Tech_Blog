2cc5a330c16b33e0c58d4b73210224c15099e94a
Dinimar Fish <tayla@tallisman.com.au>
2021-09-12 19:33:57 +1000

    added blogs to be rendered when initially visiting homepage: " "

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,4 +2,29 @@ const router = require('express').Router();
 const { Blog, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const blogData = await Blog.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const blogs = blogData.map((blog) => blog.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        blogs, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
