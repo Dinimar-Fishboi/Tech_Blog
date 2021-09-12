a3fe0916afa3e246c378be5bebc453a234cdbe40
Dinimar Fish <tayla@tallisman.com.au>
2021-09-12 21:15:18 +1000

    break

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -36,6 +36,10 @@ router.get('/', async (req, res) => {
   })
 
   router.get('/signup', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
       res.render('signup')
   })
 
