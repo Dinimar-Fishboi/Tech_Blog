0a4708424c6a2fe92e91b48c6d481270ceb96401
Dinimar Fish <tayla@tallisman.com.au>
2021-09-12 18:27:19 +1000

    get all comments block added for commentRoutes.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Comment, User, Blog } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const commentData = await Comment.findAll({
+            include: [{ model: User},  {model: Blog}]
+          })
+      
+           res.status(200).json(commentData);
+    } catch (err) {
+        res.status(450).json(err);
+    }
+})
 
 router.post('/', withAuth, async (req, res) => {
     try {
