1e1711a44d218f04fb5eb09eef151ea0c340bd33
Dinimar Fish <tayla@tallisman.com.au>
2021-09-12 18:10:43 +1000

    added datetime to comment.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -7,6 +7,7 @@ router.post('/', withAuth, async (req, res) => {
       const newComment = await Comment.create({
         ...req.body,
         blog_id: req.body.blog_id,
+        user_id: req.session.user_id,
       });
   
       res.status(200).json(newComment);
--- models/Comment.js
+++ models/Comment.js
@@ -12,13 +12,24 @@ Comment.init(
         autoIncrement: true,
     
         }, 
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.DATE
+    },
         blog_id: {
             type: DataTypes.INTEGER,      
             references: {
               model: "blog",
               key: 'id',
             }
-    },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,      
+            references: {
+              model: "user",
+              key: 'id',
+            }
+        },
     },
     {
         sequelize,
