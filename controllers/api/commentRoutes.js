da96fbd0d906d6803e5cb4b92c42555f858d9dc2
Dinimar Fish <tayla@tallisman.com.au>
2021-09-12 18:43:29 +1000

    added get/:id and put routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -14,6 +14,18 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.findByPk(req.params.id, {
+            include: [{ model: User},  {model: Blog}]
+          })
+      
+           res.status(200).json(commentData);
+    } catch (err) {
+        res.status(450).json(err);
+    }
+})
+
 router.post('/', withAuth, async (req, res) => {
     try {
       const newComment = await Comment.create({
@@ -27,6 +39,23 @@ router.post('/', withAuth, async (req, res) => {
       res.status(400).json(err);
     }
   });
+
+router.put('/:id', withAuth, async (req, res) => {
+    Comment.update(req.body, {
+        where: {
+            id: req.params.id,
+        }
+    })
+    .then((updatedComment) => {
+        if (!updatedComment) {
+            res.status(404).json({ message: 'No Comment dound with this ID'});
+        }
+        res.json(updatedComment);
+      })
+      .catch((err) => 
+      
+      res.status(500).json(err));
+})
 
   router.delete('/:id', withAuth, async (req, res) => {
     try {
