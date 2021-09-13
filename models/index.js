d0cc5040ad6cfb708df4253f9cdfa554c5b4cf66
Dinimar Fish <tayla@tallisman.com.au>
2021-09-13 11:59:10 +1000

    added false constraints to all associations

--- models/index.js
+++ models/index.js
@@ -4,36 +4,42 @@ const Blog = require('./Blog');
 
 //User can have many Blog (posts)
 User.hasMany(Blog, {
-   // constraints:false,
+   constraints:false,
    foreignKey: 'user_id',
 })
 
 //A Blog (post) will only belong to one User
 Blog.belongsTo(User, {
-    // constraints:false,
+    constraints:false,
    foreignKey: "user_id",
    onDelete: "cascade"
 })
 
 //A Blog may have many Comments
 Blog.hasMany(Comment, {
-    // constraints:false,
+    constraints:false,
 
    foreignKey: 'blog_id',
    onDelete: 'cascade'
 })
 
 Comment.belongsTo(Blog, {
+    constraints:false,
+
     foreignKey: 'blog_id',
     onDelete: 'cascade'
 })
 
 User.hasMany( Comment, {
+    constraints:false,
+
     foreignKey: 'user_id',
     onDelete: "cascade"
 })
 
 Comment.belongsTo(User, {
+    constraints:false,
+
     foreignKey: 'user_id',
     onDelete: "cascade"
 })
